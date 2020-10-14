ff88d7f008ebed4fea08d9dc892eb3899aaf3dd9
Rhythm Singh <rhysun08@gmail.com>
2020-10-14 13:05:35 +0530

    layout

---
+++ src/components/homepage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.css'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="container">
+                <div className="item">
+                    <div className="item-info">
+                        <h1 className="title">Shirts</h1>
+                        <p className="sub-title">Shop Now</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-info">
+                        <h1 className="title">Shirts</h1>
+                        <p className="sub-title">Shop Now</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-info">
+                        <h1 className="title">Shirts</h1>
+                        <p className="sub-title">Shop Now</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-info">
+                        <h1 className="title">Shirts</h1>
+                        <p className="sub-title">Shop Now</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-info">
+                        <h1 className="title">Shirts</h1>
+                        <p className="sub-title">Shop Now</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Homepage
